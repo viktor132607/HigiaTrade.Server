1b97899ce27bfc1dceb297a59406775c2af96a28
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-22 20:20:37 +0300

    Expose product price quote service contract

--- HygiaTrade.Server.Domain/Interfaces/IProductService.cs
+++ HygiaTrade.Server.Domain/Interfaces/IProductService.cs
@@ -9,9 +9,9 @@ public interface IProductService
     Task<IEnumerable<ProductResponse>?> GetAsync();
     Task<IEnumerable<ProductResponse>?> GetBestSellersAsync(int numOfBestSellers);
     Task<ProductResponse?> GetByIdAsync(Guid id);
+    Task<ProductPriceQuoteResponse> GetPriceQuoteAsync(Guid id, int quantity);
     Task<ProductResponse?> UpdateAsync(UpdateProductRequest request);
     Task<ProductResponse?> CreateAsync(CreateProductRequest request);
     Task<bool> DeleteAsync(Guid id);
     Task<Paginated<ProductsResponse>> SearchProductsAsync(SearchProductsRequest request);
-
 }
