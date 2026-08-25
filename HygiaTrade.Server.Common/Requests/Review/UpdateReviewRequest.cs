27603e04c68a5ee1b014e2079fd3847d665edffe
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-25 19:03:03 +0300

    Allow editing reviews without text

--- HygiaTrade.Server.Common/Requests/Review/UpdateReviewRequest.cs
+++ HygiaTrade.Server.Common/Requests/Review/UpdateReviewRequest.cs
@@ -6,10 +6,9 @@ public class UpdateReviewRequest
 {
     [Required]
     public required Guid Id { get; set; }
-    
-    [Required]
-    public required string Content { get; set; }
-    
-    [Required]
+
+    public string? Content { get; set; }
+
+    [Range(1, 5)]
     public required byte Rating { get; set; }
 }
