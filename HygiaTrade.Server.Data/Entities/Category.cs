91cbb54e3a8a81b618d130e9bc1ef92a58eb82e0
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-26 16:34:57 +0300

    Add category hierarchy

--- HygiaTrade.Server.Data/Entities/Category.cs
+++ HygiaTrade.Server.Data/Entities/Category.cs
@@ -4,6 +4,9 @@ namespace HygiaTrade.Data.Entities
     {
         public required string Name { get; set; }
         public required string? ImageUri { get; set; }
+        public Guid? ParentCategoryId { get; set; }
+        public Category? ParentCategory { get; set; }
+        public ICollection<Category> Subcategories { get; set; } = new List<Category>();
         public ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
