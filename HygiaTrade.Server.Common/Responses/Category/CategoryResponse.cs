c94c1c725161a02f3069f7b52932c7a2fcdc393a
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-26 16:35:27 +0300

    Expose category hierarchy

--- HygiaTrade.Server.Common/Responses/Category/CategoryResponse.cs
+++ HygiaTrade.Server.Common/Responses/Category/CategoryResponse.cs
@@ -5,4 +5,6 @@ public class CategoryResponse
     public required Guid Id { get; set; }
     public required string Name { get; set; }
     public string? ImageURI { get; set; }
+    public Guid? ParentCategoryId { get; set; }
+    public string? ParentCategoryName { get; set; }
 }
